e9633707dc0e7c7bc2115d0de216ebd19650ec4e
Bram Janssen <ibramsterdam@hotmail.com>
2022-10-22 02:59:05 +0200

    Solution for 219

--- 1-two-sum/solution.ts
+++ 1-two-sum/solution.ts
@@ -1,3 +1,4 @@
+// Time: 196 ms (27.9%), Memory: 44.3 MB (90.94%)
 function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; i++) {
     for (let k = 0; k < nums.length; k++) {
