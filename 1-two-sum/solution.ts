c4027f452da3c5d74938fd5bd0329c9bbdee5eb1
Bram Janssen <ibramsterdam@hotmail.com>
2022-10-22 02:38:52 +0200

    Init

---
+++ 1-two-sum/solution.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let k = 0; k < nums.length; k++) {
+      if (nums[i] + nums[k] === target) {
+        if (i !== k) {
+          return [i, k];
+        }
+      }
+    }
+  }
+  return [-1, -1];
+}
+a
