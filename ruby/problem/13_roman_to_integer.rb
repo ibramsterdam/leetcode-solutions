3fdf7ddeed90b5ea1f79525720652d3fff71a778
Bram Janssen <ibramsterdam@hotmail.com>
2023-10-07 22:21:57 +0200

    roman to integer

--- ruby/problem/13_roman_to_integer.rb
+++ ruby/problem/13_roman_to_integer.rb
@@ -1,8 +1,22 @@
+require 'pry-byebug'
 # @param {String} s
 # @return {Integer}
 def roman_to_int(s)
-    chars = {"I": 1, "V": 5 , "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
+    roman_vals = {"I" => 1, "V"=> 5 , "X"=> 10, "L"=> 50, "C"=> 100, "D"=> 500, "M"=> 1000 }
+    result = 0 
+    prev_value = 0
 
+    s.reverse.each_char do |char|
+       value = roman_vals[char] 
+       if value >= prev_value 
+        result += value 
+       else 
+        result -= value
+       end
+
+       prev_value = value
+    end
+    result
 end
 
 
